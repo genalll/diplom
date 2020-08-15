b0ba2679787e98528799c0d2c0e66be5a8c3709a
genalll <genalll@mail.ru>
2020-08-15 16:20:08 +0500

    Сделал месяц на странице аналитики.

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -2,30 +2,32 @@ import "../vendor/normalise.css";
 import "../page/analitikastyle.css";
 import DataStorage from "../js/modules/datastorage.js";
 import Analitika from "../js/components/analitika.js";
-import {checkTitleResult,checkTotalResult,breackArrFordata,percentDay,getDataInterval,getDataStr,sortMassivDays} from "../js/utelites/analitikautilits.js"
+import { checkTitleResult, checkTotalResult, breackArrFordata, percentDay, getDataInterval, getDataStr, sortMassivDays, getMonth } from "../js/utelites/analitikautilits.js"
 const DataStorages = new DataStorage();
 const nameSearch = document.querySelector(".analitika-title__title");
 const totalResult = document.querySelector("#totalresuilt");
 const totalResultTitle = document.querySelector("#totalresuiltitle");
 const nevsArrforCheck = DataStorages.getTolocalStorage("nevsArrForData");
-const nameSearchValue =DataStorages.getTolocalStorage("name");
+const nameSearchValue = DataStorages.getTolocalStorage("name");
 const analitikaTableWeek = document.querySelectorAll(".analitika-table__week-day");
 const analitikaTabldayValue = document.querySelectorAll(".analitika-table__scale");
-if (nameSearchValue){
-nameSearch.textContent=nameSearchValue;
+const fiveWeak = document.querySelector("#month")
+if (nameSearchValue) {
+    nameSearch.textContent = nameSearchValue;
 };
-if (nevsArrforCheck){
+if (nevsArrforCheck) {
     totalResult.textContent = nevsArrforCheck.length;
 }
-totalResult.textContent=checkTotalResult(nevsArrforCheck,nameSearchValue);
-totalResultTitle.textContent=(checkTitleResult(nevsArrforCheck,nameSearchValue));
-const MassivOfweekDay = (percentDay(breackArrFordata(nevsArrforCheck),nevsArrforCheck))[0];
-const percentMassivOfweekDay = (percentDay(breackArrFordata(nevsArrforCheck),nevsArrforCheck))[1];
+totalResult.textContent = checkTotalResult(nevsArrforCheck, nameSearchValue);
+totalResultTitle.textContent = (checkTitleResult(nevsArrforCheck, nameSearchValue));
+fiveWeak.textContent = getMonth();
+const MassivOfweekDay = (percentDay(breackArrFordata(nevsArrforCheck), nevsArrforCheck))[0];
+const percentMassivOfweekDay = (percentDay(breackArrFordata(nevsArrforCheck), nevsArrforCheck))[1];
 const weckDayNumberDay = getDataInterval();
 const dayAndWeek = getDataStr();
-const totalOfDayMassiv = sortMassivDays(MassivOfweekDay,weckDayNumberDay);
-const totalOfDaypercent = sortMassivDays(percentMassivOfweekDay,weckDayNumberDay);
-const analitikas= new Analitika(dayAndWeek,totalOfDayMassiv,totalOfDaypercent,analitikaTableWeek,analitikaTabldayValue);
+const totalOfDayMassiv = sortMassivDays(MassivOfweekDay, weckDayNumberDay);
+const totalOfDaypercent = sortMassivDays(percentMassivOfweekDay, weckDayNumberDay);
+const analitikas = new Analitika(dayAndWeek, totalOfDayMassiv, totalOfDaypercent, analitikaTableWeek, analitikaTabldayValue);
 analitikas.renderWeekday();
 analitikas.renderTotalvalueDay();
 analitikas.renderPircentDay();
--- src/js/utelites/analitikautilits.js
+++ src/js/utelites/analitikautilits.js
@@ -110,4 +110,25 @@ function getDataInterval(){
     return sortArr
 }
 
-export{checkTitleResult,checkTotalResult,breackArrFordata,percentDay,getDataInterval,getDataStr,sortMassivDays};
+function getMonth(){
+    let datae = new Date();
+    let  Month = datae.getMonth();
+    let fMonth="";
+        switch (Month) {
+            case 0: fMonth = "января"; break;
+            case 1: fMonth = "февраля"; break;
+            case 2: fMonth = "марта"; break;
+            case 3: fMonth = "апреля"; break;
+            case 4: fMonth = "мая"; break;
+            case 5: fMonth = "июня"; break;
+            case 6: fMonth = "июля"; break;
+            case 7: fMonth = "августа"; break;
+            case 8: fMonth = "сентября"; break;
+            case 9: fMonth = "октября"; break;
+            case 10: fMonth = "ноября"; break;
+            case 11: fMonth = "декабря"; break;
+        }
+        return "("+fMonth+")"
+};
+
+export{checkTitleResult,checkTotalResult,breackArrFordata,percentDay,getDataInterval,getDataStr,sortMassivDays,getMonth};
